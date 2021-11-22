a47ae85a3f4b105744acc6b20018f31681b159e1
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-11-22 08:27:22 +0000

    Update init.lua

--- worldmods/exploration/init.lua
+++ worldmods/exploration/init.lua
@@ -10,6 +10,23 @@ local jailradius = 10;
 
 local freepos = {x=-900, y=24, z=-1189};
 
+
+-- bls rollbeck check
+minetest.register_privilege("rollback_check", "Allows use of /rollback_check")
+minetest.override_chatcommand("rollback_check", {
+    privs = {rollback_check=true}
+})
+-- allow regular players access to limited rollback checking
+minetest.register_chatcommand("grief_check", {
+    description = "Check who last touched a node or a node near it",
+    func = function(name)
+        return minetest.registered_chatcommands["rollback_check"].func(
+            name,
+            "1 1209600 10"
+        )
+    end
+})
+
 -- spawn command
 spawn_command = {}
 spawn_command.pos = {-900,24,-1189}
