75c7870598c7a9532faa32aaf121dd1abc4a4bf2
mrbid <james@voxdsp.com>
2021-12-03 10:11:09 +0000

    general

--- worldmods/Minetest-WorldEditAdditions/worldeditadditions/utils/format/init.lua
+++
@@ -1,8 +0,0 @@
-worldeditadditions.format = {}
-
-dofile(worldeditadditions.modpath.."/utils/format/array_2d.lua")
-dofile(worldeditadditions.modpath.."/utils/format/node_distribution.lua")
-dofile(worldeditadditions.modpath.."/utils/format/make_ascii_table.lua")
-dofile(worldeditadditions.modpath.."/utils/format/map.lua")
-dofile(worldeditadditions.modpath.."/utils/format/human_time.lua")
-dofile(worldeditadditions.modpath.."/utils/format/human_size.lua")
