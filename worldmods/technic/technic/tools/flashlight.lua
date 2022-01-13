7c559b8185fe276451f36f55eb58a34bac4350f8
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-01-13 18:21:23 +0000

    Update flashlight.lua

--- worldmods/technic/technic/tools/flashlight.lua
+++ worldmods/technic/technic/tools/flashlight.lua
@@ -36,17 +36,21 @@ local function check_for_flashlight(player)
 	end
 	local inv = player:get_inventory()
 	local hotbar = inv:get_list("main")
-	for i = 1, 8 do
-		if hotbar[i]:get_name() == "technic:flashlight" then
-			local meta = minetest.deserialize(hotbar[i]:get_metadata())
-			if meta and meta.charge and meta.charge >= 2 then
-				if not technic.creative_mode then
-					meta.charge = meta.charge - 2;
-					technic.set_RE_wear(hotbar[i], meta.charge, flashlight_max_charge)
-					hotbar[i]:set_metadata(minetest.serialize(meta))
-					inv:set_stack("main", i, hotbar[i])
+	if hotbar ~= nil then
+		for i = 1, 32 do
+			if hotbar[i] ~= nil then
+				if hotbar[i]:get_name() == "technic:flashlight" then
+					local meta = minetest.deserialize(hotbar[i]:get_metadata())
+					if meta and meta.charge and meta.charge >= 2 then
+						if not technic.creative_mode then
+							meta.charge = meta.charge - 2;
+							technic.set_RE_wear(hotbar[i], meta.charge, flashlight_max_charge)
+							hotbar[i]:set_metadata(minetest.serialize(meta))
+							inv:set_stack("main", i, hotbar[i])
+						end
+						return true
+					end
 				end
-				return true
 			end
 		end
 	end
