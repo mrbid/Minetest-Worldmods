5b0c388b64197c9db79a13eb0df5159c815172a9
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-01-18 03:24:45 +0000

    Update nodes.lua

--- worldmods/mychisel/nodes.lua
+++ worldmods/mychisel/nodes.lua
@@ -198,31 +198,31 @@ local squar4 = {
 	}
 
 local default_material = {
-		{"default:cobble", "default_cobble", "Cobble", {cracky = 3, not_in_creative_inventory=1}},
-		{"default:sandstone","default_sandstone", "Sandstone", {crumbly=2, not_in_creative_inventory=1}},
-		{"default:clay","default_clay",  "Clay", {crumbly=3, not_in_creative_inventory=1}},
-		{"default:coalblock","default_coal_block",  "Coal Block", {cracky = 3, not_in_creative_inventory=1}},
-		{"default:stone","default_stone", "Stone", {cracky = 3, not_in_creative_inventory=1}},
-		{"default:desert_stone","default_desert_stone", "Desert Stone", {cracky = 3, not_in_creative_inventory=1}},
-		{"default:wood","default_wood", "Wood", {choppy=2, not_in_creative_inventory=1}},
-		{"default:acacia_wood","default_acacia_wood", "Acacia Wood", {choppy=2, not_in_creative_inventory=1}},
-		{"default:aspen_wood","default_aspen_wood", "Aspen Wood", {choppy=2, not_in_creative_inventory=1}},
-		{"default:pine_wood","default_pine_wood", "Pine Wood", {choppy=2, not_in_creative_inventory=1}},
-		{"default:desert_cobble","default_desert_cobble", "Desert Cobble", {cracky = 3, not_in_creative_inventory=1}},
-		{"default:junglewood","default_junglewood", "Jungle Wood", {choppy=2, not_in_creative_inventory=1}},
-		{"default:sandstonebrick","default_sandstone_brick", "Sandstone Brick", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:stonebrick","default_stone_brick", "Stone Brick", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:desert_stonebrick","default_desert_stone_brick", "Desert Stone Brick", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:steelblock", "default_steel_block", "Steel Block", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:copperblock", "default_copper_block", "Copper Block", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:bronzeblock", "default_bronze_block", "Bronze Block", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:goldblock", "default_gold_block", "Gold Block", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:tinblock", "default_tin_block", "Tin Block", {cracky = 2, not_in_creative_inventory=1}},
-		{"moreblocks:copperpatina", "moreblocks_copperpatina", "Copperpatina", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:desert_sandstone","default_desert_sandstone", "Desert Sandstone", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:desert_sandstone_brick","default_desert_sandstone_brick", "Desert Sandstonebrick", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:silver_sandstone","default_silver_sandstone", "Silver Sandstone", {cracky = 2, not_in_creative_inventory=1}},
-		{"default:silver_sandstone_brick","default_silver_sandstone_brick", "Silver Sandstonebrick", {cracky = 2, not_in_creative_inventory=1}},
+		{"default:cobble", "default_cobble", "Cobble", {cracky = 3}},
+		{"default:sandstone","default_sandstone", "Sandstone", {crumbly=2}},
+		{"default:clay","default_clay",  "Clay", {crumbly=3}},
+		{"default:coalblock","default_coal_block",  "Coal Block", {cracky = 3}},
+		{"default:stone","default_stone", "Stone", {cracky = 3}},
+		{"default:desert_stone","default_desert_stone", "Desert Stone", {cracky = 3}},
+		{"default:wood","default_wood", "Wood", {choppy=2}},
+		{"default:acacia_wood","default_acacia_wood", "Acacia Wood", {choppy=2}},
+		{"default:aspen_wood","default_aspen_wood", "Aspen Wood", {choppy=2}},
+		{"default:pine_wood","default_pine_wood", "Pine Wood", {choppy=2}},
+		{"default:desert_cobble","default_desert_cobble", "Desert Cobble", {cracky = 3}},
+		{"default:junglewood","default_junglewood", "Jungle Wood", {choppy=2}},
+		{"default:sandstonebrick","default_sandstone_brick", "Sandstone Brick", {cracky = 2}},
+		{"default:stonebrick","default_stone_brick", "Stone Brick", {cracky = 2}},
+		{"default:desert_stonebrick","default_desert_stone_brick", "Desert Stone Brick", {cracky = 2}},
+		{"default:steelblock", "default_steel_block", "Steel Block", {cracky = 2}},
+		{"default:copperblock", "default_copper_block", "Copper Block", {cracky = 2}},
+		{"default:bronzeblock", "default_bronze_block", "Bronze Block", {cracky = 2}},
+		{"default:goldblock", "default_gold_block", "Gold Block", {cracky = 2}},
+		{"default:tinblock", "default_tin_block", "Tin Block", {cracky = 2}},
+		{"moreblocks:copperpatina", "moreblocks_copperpatina", "Copperpatina", {cracky = 2}},
+		{"default:desert_sandstone","default_desert_sandstone", "Desert Sandstone", {cracky = 2}},
+		{"default:desert_sandstone_brick","default_desert_sandstone_brick", "Desert Sandstonebrick", {cracky = 2}},
+		{"default:silver_sandstone","default_silver_sandstone", "Silver Sandstone", {cracky = 2}},
+		{"default:silver_sandstone_brick","default_silver_sandstone_brick", "Silver Sandstonebrick", {cracky = 2}},
 		}
 
 
@@ -247,7 +247,7 @@ if minetest.get_modpath( "bakedclay") then
 	}
 
 	for _, clay in pairs(clay) do
-		table.insert(default_material,{"bakedclay:"..clay[1] , "baked_clay_" .. clay[1], clay[2] .. " Baked Clay",{cracky = 2, not_in_creative_inventory=1}})
+		table.insert(default_material,{"bakedclay:"..clay[1] , "baked_clay_" .. clay[1], clay[2] .. " Baked Clay",{cracky = 2}})
 	end
 end
 
