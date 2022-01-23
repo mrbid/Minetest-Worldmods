c897ca069d44bc5e3477b5ba7cb8bbfb20cee1f5
mrbid <james@voxdsp.com>
2022-01-23 10:37:16 +0000

    general

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
-for file in worldmods/*.png worldmods/**/*.png ; do
+for file in /home/v/Desktop/worldmods/*.png /home/v/Desktop/worldmods/**/textures/*.png /home/v/Desktop/worldmods/**/*.png ; do
   trimage -f "$file"
 done;
