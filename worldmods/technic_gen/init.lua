0bf114f336b98c9c215fcb4cecb018d0a0ceee1f
mrbid <james@voxdsp.com>
2022-01-25 12:46:18 +0000

    general

--- worldmods/technic_gen/init.lua
+++ worldmods/technic_gen/init.lua
@@ -4,9 +4,6 @@
 
 local S = technic.getter
 
-local fs_helpers = pipeworks.fs_helpers
-local tube_entry = "^pipeworks_tube_connection_metallic.png"
-
 local function round(v)
 	return math.floor(v + 0.5)
 end
@@ -25,11 +22,7 @@ function register_gen(data)
 
 	data.modname = minetest.get_current_modname()
 
-	local groups = {cracky = 2, technic_machine = 1, ["technic_"..ltier] = 1}
-	if data.tube then
-		groups.tubedevice = 1
-		groups.tubedevice_receiver = 1
-	end
+	local groups = {cracky = 2, tubedevice = 1, technic_machine = 1, ["technic_"..ltier] = 1}
 	local active_groups = {not_in_creative_inventory = 1}
 	for k, v in pairs(groups) do active_groups[k] = v end
 
@@ -53,14 +46,6 @@ function register_gen(data)
 			"listring[current_player;main]"
 	end
 
-	local tube = technic.new_default_tube()
-	if data.can_insert then
-		tube.can_insert = data.can_insert
-	end
-	if data.insert_object then
-		tube.insert_object = data.insert_object
-	end
-
 	local run = function(pos, node)
 		local meta     = minetest.get_meta(pos)
 		local inv      = meta:get_inventory()
@@ -77,12 +62,9 @@ function register_gen(data)
 			return
 		end
 
-		local EU_upgrade, tube_upgrade = 0, 0
+		local EU_upgrade = 0
 		if data.upgrade then
-			EU_upgrade, tube_upgrade = technic.handle_machine_upgrades(meta)
-		end
-		if data.tube then
-			technic.handle_machine_pipeworks(pos, tube_upgrade)
+			EU_upgrade = technic.handle_machine_upgrades(meta)
 		end
 
 		meta:set_int(tier.."_EU_demand", machine_demand[EU_upgrade+1])
@@ -101,7 +83,8 @@ function register_gen(data)
 		if meta:get_int("src_time") >= data.speed then
 			meta:set_int("src_time", 0)
 
-			local output = 'default:cobble ' .. outamount
+			local strout = 'default:cobble ' .. outamount
+			local output = strout
 			if type(output) ~= "table" then output = { output } end
 			local output_stacks = {}
 			for _, o in ipairs(output) do
@@ -120,7 +103,9 @@ function register_gen(data)
 			if not room_for_output then
 				technic.swap_node(pos, machine_node)
 				meta:set_string("infotext", S("%s Finished"):format(machine_desc_tier))
-				meta:set_int("src_time", 0)
+				--meta:set_int("src_time", 0)
+				--minetest.add_item({x=pos.x, y=pos.y+1, z=pos.z}, strout)
+				technic.tube_inject_item(pos, pos, vector.new(0, 1, 0), strout)
 				return
 			end
 
@@ -130,24 +115,21 @@ function register_gen(data)
 
 	end
 
-	local tentry = tube_entry
-	if ltier == "lv" then
-		tentry = ""
-	end
-
 	minetest.register_node(data.modname..":"..ltier.."_"..machine_name, {
 		description = machine_desc:format(tier),
 		tiles = {
-			data.modname.."_"..ltier.."_"..machine_name.."_top.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_bottom.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_right.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_left.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_back.png"..tentry,
+			data.modname.."_"..ltier.."_"..machine_name.."_top.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_bottom.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_right.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_left.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_back.png",
 			data.modname.."_"..ltier.."_"..machine_name.."_front.png"
 		},
 		paramtype2 = "facedir",
 		groups = groups,
-		tube = data.tube and tube or nil,
+		tube = {
+			connect_sides = {top = 1},
+		},
 		connect_sides = data.connect_sides or connect_default,
 		legacy_facedir_simple = true,
 		sounds = default.node_sound_wood_defaults(),
@@ -156,20 +138,8 @@ function register_gen(data)
 			local meta = minetest.get_meta(pos)
 
 			local form_buttons = ""
-			if not string.find(node.name, ":lv_") then
-				form_buttons = fs_helpers.cycling_button(
-					meta,
-					pipeworks.button_base,
-					"splitstacks",
-					{
-						pipeworks.button_off,
-						pipeworks.button_on
-					}
-				)..pipeworks.button_label
-			end
 
 			meta:set_string("infotext", machine_desc:format(tier))
-			meta:set_int("tube_time",  0)
 			meta:set_string("formspec", formspec..form_buttons)
 			local inv = meta:get_inventory()
 			inv:set_size("dst", slots)
@@ -181,38 +151,17 @@ function register_gen(data)
 		allow_metadata_inventory_take = technic.machine_inventory_take,
 		allow_metadata_inventory_move = technic.machine_inventory_move,
 		technic_run = run,
-		after_place_node = data.tube and pipeworks.after_place,
 		after_dig_node = technic.machine_after_dig_node,
-		on_receive_fields = function(pos, formname, fields, sender)
-			if fields.quit then return end
-			if not pipeworks.may_configure(pos, sender) then return end
-			fs_helpers.on_receive_fields(pos, fields)
-			local node = minetest.get_node(pos)
-			local meta = minetest.get_meta(pos)
-			local form_buttons = ""
-			if not string.find(node.name, ":lv_") then
-				form_buttons = fs_helpers.cycling_button(
-					meta,
-					pipeworks.button_base,
-					"splitstacks",
-					{
-						pipeworks.button_off,
-						pipeworks.button_on
-					}
-				)..pipeworks.button_label
-			end
-			meta:set_string("formspec", formspec..form_buttons)
-		end,
 	})
 
 	minetest.register_node(data.modname..":"..ltier.."_"..machine_name.."_active",{
 		description = machine_desc:format(tier),
 		tiles = {
-			data.modname.."_"..ltier.."_"..machine_name.."_top.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_bottom.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_right.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_left.png"..tentry,
-			data.modname.."_"..ltier.."_"..machine_name.."_back.png"..tentry,
+			data.modname.."_"..ltier.."_"..machine_name.."_top.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_bottom.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_right.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_left.png",
+			data.modname.."_"..ltier.."_"..machine_name.."_back.png",
 			data.modname.."_"..ltier.."_"..machine_name.."_front_active.png"
 		},
 		paramtype2 = "facedir",
@@ -221,33 +170,12 @@ function register_gen(data)
 		connect_sides = data.connect_sides or connect_default,
 		legacy_facedir_simple = true,
 		sounds = default.node_sound_wood_defaults(),
-		tube = data.tube and tube or nil,
 		can_dig = technic.machine_can_dig,
 		allow_metadata_inventory_put = technic.machine_inventory_put,
 		allow_metadata_inventory_take = technic.machine_inventory_take,
 		allow_metadata_inventory_move = technic.machine_inventory_move,
 		technic_run = run,
 		technic_disabled_machine_name = data.modname..":"..ltier.."_"..machine_name,
-		on_receive_fields = function(pos, formname, fields, sender)
-			if fields.quit then return end
-			if not pipeworks.may_configure(pos, sender) then return end
-			fs_helpers.on_receive_fields(pos, fields)
-			local node = minetest.get_node(pos)
-			local meta = minetest.get_meta(pos)
-			local form_buttons = ""
-			if not string.find(node.name, ":lv_") then
-				form_buttons = fs_helpers.cycling_button(
-					meta,
-					pipeworks.button_base,
-					"splitstacks",
-					{
-						pipeworks.button_off,
-						pipeworks.button_on
-					}
-				)..pipeworks.button_label
-			end
-			meta:set_string("formspec", formspec..form_buttons)
-		end,
 	})
 
 	technic.register_machine(tier, data.modname..":"..ltier.."_"..machine_name,            technic.receiver)
@@ -282,10 +210,10 @@ minetest.register_craft({
 	}
 })
 
-register_gen({name= "stone", tier = "LV", demand = {1000, 900, 800}, speed = 5.0, amount = 1, slots = 1, upgrade = 0, tube = 0})
+register_gen({name= "stone", tier = "LV", demand = {1000, 900, 800}, speed = 5.0, amount = 1, slots = 1, upgrade = 0})
 
-register_gen({name= "iron", tier = "MV", demand = {2000, 1850, 1700}, speed = 1.0, amount = 1, slots = 4, upgrade = 1, tube = 1})
-register_gen({name= "diamond", tier = "MV", demand = {7500, 7000, 6500}, speed = 1.0, amount = 10, slots = 8, upgrade = 1, tube = 1})
+register_gen({name= "iron", tier = "MV", demand = {2000, 1850, 1700}, speed = 1.0, amount = 1, slots = 4, upgrade = 1})
+register_gen({name= "diamond", tier = "MV", demand = {7500, 7000, 6500}, speed = 1.0, amount = 10, slots = 16, upgrade = 1})
 
-register_gen({name= "mithril", tier = "HV", demand = {25000, 24000, 23000}, speed = 1.0, amount = 50, slots = 16, upgrade = 1, tube = 1})
-register_gen({name= "osmium", tier = "HV", demand = {100000, 95000, 90000}, speed = 1.0, amount = 250, slots = 16, upgrade = 1, tube = 1})
+register_gen({name= "mithril", tier = "HV", demand = {25000, 24000, 23000}, can_insert = "dst", speed = 1.0, amount = 50, slots = 16, upgrade = 1})
+register_gen({name= "osmium", tier = "HV", demand = {100000, 95000, 90000}, speed = 1.0, amount = 250, slots = 16, upgrade = 1})
