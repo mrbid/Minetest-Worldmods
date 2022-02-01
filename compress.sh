8fbefd17e5f10b654dab83221644c47d7d639d39
mrbid <james@voxdsp.com>
2022-02-01 16:41:49 +0000

    general

--- compress.sh
+++ compress.sh
@@ -1,3 +1,4 @@
+# warning this could break some textures adding black pixels in the corners or just breaking any transparency they might have had
 for file in /home/v/Desktop/worldmods/*.png /home/v/Desktop/worldmods/**/textures/*.png /home/v/Desktop/worldmods/**/*.png ; do
   trimage -f "$file"
 done;
