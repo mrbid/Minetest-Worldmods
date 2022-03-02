8e9f7e5d068691be1b0708e78f75bae2f3a9b687
mrbid <james@voxdsp.com>
2022-03-02 16:09:58 +0000

    general

---
+++ exiledmods/OTHER/meh/vfc/init.lua
@@ -0,0 +1,16 @@
+minetest.register_craftitem("vfc:vf", {
+	description = "$$$",
+	inventory_image = "vfc.png",
+	stack_max = 65535,
+	groups = {not_in_creative_inventory=1}
+})
+
+-- minetest.register_alias("currency:minegeld_cent_5", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_cent_10", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_cent_25", "vfc:vf")
+-- minetest.register_alias("currency:minegeld", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_5", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_10", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_50", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_100", "vfc:vf")
+-- minetest.register_alias("currency:minegeld_bundle", "vfc:vf")
