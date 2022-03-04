f3835ac1124d7444c9aa4e3e1a6fe38506ce4707
mrbid <james@voxdsp.com>
2022-03-04 06:51:25 +0000

    general

--- exiledmods/good/no_pipeworks/technic_no_pipeworks/wrench/README.md
+++
@@ -1,2 +0,0 @@
-James William Fletcher (https://github.com/mrbid/Minetest-Worldmods)
-  - Fixed for use by players, now craftable. Server crashes fixed.
--- exiledmods/good/no_pipeworks/xtraores_greenxenith_no_pipeworks/xtraores_guns/init.lua
+++
@@ -1,8 +0,0 @@
---Do the things
-dofile(minetest.get_modpath("xtraores_guns").."/guns.lua")
-dofile(minetest.get_modpath("xtraores_guns").."/bullets.lua")
-
-minetest.log("action", "[MOD] Xtraores: Module (guns) loaded!")
-
-
---Things done! \o/
